94ddb2e0b5aa6329c6715be9291824378114c08d
AbdullohKomil <akomiljonov@icloud.com>
2023-08-10 16:58:20 +0500

    hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { Header } from './components/Header/Header';
 import { Home } from './pages/Home/Home';
 import './assets/styles/index.css';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 function App() {
   return (
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
+import { Hero } from '../../components/Hero/Hero'
 
 export const Home = () => {
   return (
-    <></>
+    <>
+      <Hero></Hero>
+    
+    </>
   )
 }
